52f1e41cadbdb6b8fc8282793084dc500fb92efc
iCallH4x <64992884+iCallH4x@users.noreply.github.com>
2021-11-02 00:42:01 +0000

    Fixed messy code (ish)
    Just wanted to make things look cleaner and more streamline

--- server/main.lua
+++ server/main.lua
@@ -3,6 +3,7 @@
  RegisterCommand('tweet', function(source, args, rawCommand)
     local playerName = GetPlayerName(source)
     local msg = rawCommand:sub(6)
+		
     TriggerClientEvent('chat:addMessage', -1, {
         template = '<div style="padding: 0.5vw; margin: 0.5vw; background-color: rgba(28, 160, 242, 0.6); border-radius: 3px;"> <img src="https://i.imgur.com/rj3Ex7T.png" align="top"> @{0}:<br> {1}</div>',
         args = { playerName, msg }
